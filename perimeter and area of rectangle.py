0e896bc3e16e91c89aa66cd13a9e7b315e135e19
2203031260200 <157494138+2203031260200@users.noreply.github.com>
2024-01-24 00:00:11 +0530

    Update perimeter and area of rectangle.py

--- perimeter and area of rectangle.py
+++ perimeter and area of rectangle.py
@@ -1,7 +1,8 @@
-#calculating perimeter and area of rectangle
+fh=open("output 2.txt",'w')
 l=int(input("enter the value of length:"))
 b=int(input("enter the value of breadth:"))
 perimeter=2*(l+b)
 area-l*b
-print("the perimeter of rectangle :",perimeter)
-print("the area of rectangle :",area)
+fh.write("the perimeter of rectangle :"+str(perimeter))
+fh.write("the area of rectangle :"+str(area))
+fh.close
